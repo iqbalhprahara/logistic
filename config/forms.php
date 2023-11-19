df48bf7c0c4cdd2149164eb57007991e0f95b591
iprahara <117567922+iprahara@users.noreply.github.com>
2023-11-19 19:48:06 +0700

    hotfix: add workaround for enabling form dark mode for icetalker/filament-stepper (#7)

---
+++ config/forms.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Work around for enabling dark mode for package icetalker/filament-stepper
+|--------------------------------------------------------------------------
+*/
+return [
+    'dark_mode' => true,
+];
